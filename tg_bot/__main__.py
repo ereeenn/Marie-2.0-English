2e52d000cb56014ad9e729afc54ea330a337182e
ereeenn <67025874+ereeenn@users.noreply.github.com>
2020-06-17 21:50:27 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -140,7 +140,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN)
     else:
-        update.effective_message.reply_text("")
+        update.effective_message.reply_text("Selam! :) beni nasıl kullanacağınız hakkında herhangi bir sorunuz varsa bana PM'den yazın!")
 
 
 # for test purposes
